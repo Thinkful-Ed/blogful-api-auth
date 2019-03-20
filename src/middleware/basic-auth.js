76e4e8084b12beee94e8ffd7b4408ca43414e008
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-03-20 20:05:45 +0000

    fixes use of basic auth

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -3,14 +3,14 @@ const AuthService = require('../auth/auth-service')
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
 
-  let bearerToken
-  if (!authToken.toLowerCase().startsWith('bearer ')) {
-    return res.status(401).json({ error: 'Missing bearer token' })
+  let basicToken
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'Missing basic token' })
   } else {
-    bearerToken = authToken.slice(7, authToken.length)
+    basicToken = authToken.slice('basic '.length, authToken.length)
   }
 
-  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(bearerToken)
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
 
   if (!tokenUserName || !tokenPassword) {
     return res.status(401).json({ error: 'Unauthorized request' })
