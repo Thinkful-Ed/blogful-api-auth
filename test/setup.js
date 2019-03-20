9cd0358cd17c3bba5488e737840375c4f5e26e1e
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-03-20 19:16:08 +0000

    maintain sequence numbers for tables in tests that insert fixtures with ids

--- test/setup.js
+++ test/setup.js
@@ -2,6 +2,10 @@ process.env.TZ = 'UCT'
 process.env.NODE_ENV = 'test'
 
 require('dotenv').config()
+
+process.env.TEST_DB_URL = process.env.TEST_DB_URL
+  || "postgresql://dunder-mifflin@localhost/blogful-auth-test"
+
 const { expect } = require('chai')
 const supertest = require('supertest')
 
