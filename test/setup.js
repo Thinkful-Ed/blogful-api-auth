48e3d2ad31d96a31776f242f1866b20c588ceaa4
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-09-18 10:13:59 +0100

    using dunder_mifflin and added a `npm run migrate:test` command

--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,7 @@ process.env.JWT_SECRET = 'test-jwt-secret'
 require('dotenv').config()
 
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://dunder-mifflin@localhost/blogful-auth-test"
+  || "postgresql://dunder_mifflin@localhost/blogful-auth-test"
 
 const { expect } = require('chai')
 const supertest = require('supertest')
